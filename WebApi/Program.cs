79f1d41bb906d762f7f68359085dff5da9935130
lorinalbrecht <lorin@cmweb.ch>
2024-05-22 15:09:24 +0200

    app settings

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,20 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var movieDatabaseConfigSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(movieDatabaseConfigSection);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) => {
+    string mongoConnection = options.Value.ConnectionString;
+    var mongoClient = new MongoClient(mongoConnection);
+    var databaseNames = mongoClient.ListDatabaseNames().ToList();
+
+    return string.Join(",", databaseNames);
+});
 
 app.Run();
